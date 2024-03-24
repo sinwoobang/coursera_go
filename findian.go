c03e2b91e34954d4c3cef8d24de37f3a6829a836
Sin-Woo Bang <sinwoobang@gmail.com>
2024-03-25 00:50:21 +0900

    Fix bug

--- findian.go
+++ findian.go
@@ -10,8 +10,8 @@ import (
 func main() {
     inputReader := bufio.NewReader(os.Stdin)
     input, _ := inputReader.ReadString('\n')
-	input = strings.ToLower(input);
-	t := strings.Contains(input, "i") && strings.Contains(input, "a") && strings.Contains(input, "n")
+	input = strings.TrimRight(strings.ToLower(input), "\n");
+	t := strings.HasPrefix(input, "i") && strings.Contains(input, "a") && strings.HasSuffix(input, "n")
 	if t {
 		fmt.Println("Found!")
 	} else {
